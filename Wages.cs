0ed286e69bca1795270508b896e6403c1d8372d4
Yassine Ghannay <109186232+yassineghannay@users.noreply.github.com>
2024-01-30 16:03:55 -0700

    Update Wages.cs

--- Wages.cs
+++ Wages.cs
@@ -1,1 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Inheritance
+{
+    internal class Wages : Employee
+    {
+        public double rate { get; set; }
+        public double hours { get; set; }
+
+
+        public double GetPay(double rate , double hours , double newRate)
+        {
+
+
+            newRate = rate * 0.5;
+            if (hours > 40 )
+            {
+                return newRate;
+            } else
+            {
+                return rate * hours;
+            }
+
+
+        }
+
+
+       
+
+
+
+
+
+    }
+}
 
