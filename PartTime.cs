e6a2976f5938afde08f8d057e6f41d0c11ac7bce
Yassine Ghannay <109186232+yassineghannay@users.noreply.github.com>
2024-01-30 16:06:48 -0700

    Update PartTime.cs

--- PartTime.cs
+++ PartTime.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Inheritance
+{
+    internal class PartTime : Employee
+    {
+
+        public double rate { get; set; }
+        public double hours { get; set;  }
+
+
+        public double GetPay(double rate, double hours)
+        {
+           
+            return rate * hours;
+
+        }
+
+
+     
+
+    }
+}
 
