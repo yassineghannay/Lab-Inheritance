14d6a828c55ba25850f8f7401c4b884a6c96182d
Yassine Ghannay <109186232+yassineghannay@users.noreply.github.com>
2024-01-30 16:10:13 -0700

    Update Salaried.cs

--- Salaried.cs
+++ Salaried.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Inheritance
+{
+    // git test
+    internal class Salaried : Employee
+    {
+
+     public double salary { get; set;  }
+
+        public double GetPay(double salary)
+        { 
+            return salary;
+              
+            
+        }
+
+     
+
+
+    }
+}
 
