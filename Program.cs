f4d4e3a6b80b8f264801571aee39edbfe6597b8e
Yassine Ghannay <109186232+yassineghannay@users.noreply.github.com>
2024-01-30 16:02:39 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hello World!");
+            
+            
+            Console.ReadLine();
+
+        }
+    }
+}
 
